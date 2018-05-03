1e0e025a6746ebe04f2ab9a2d36cc3e0e290672d
invMou <shopir9@hotmail.com>
2018-05-03 11:17:40 -0500

    models relationship created

--- app/models/card_copy.rb
+++ app/models/card_copy.rb
@@ -1,2 +1,5 @@
 class CardCopy < ApplicationRecord
+  has_many :card_decks
+  belongs_to :player
+  belongs_to :card
 end
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,2 +1,4 @@
 class Deck < ApplicationRecord
+  belongs_to :player
+  has_many :card_decks
 end
